1da1f2947a0c69d65aba3fb840ae3428bc29659e
Francois-Xavier 'FiX' KOWALSKI <francois-xavier.kowalski@hp.com>
2018-04-22 09:31:29 +0200

    Testify guestprop

--- guestprop_test.go
+++ guestprop_test.go
@@ -1,9 +1,10 @@
 package virtualbox
 
 import (
-	"fmt"
 	"os"
 	"testing"
+
+	"github.com/stretchr/testify/assert"
 )
 
 func TestGuestProperty(t *testing.T) {
@@ -15,39 +16,30 @@ func TestGuestProperty(t *testing.T) {
 	t.Logf("Using '%s'", vm)
 
 	err := SetGuestProperty(vm, "test_key", "test_val")
-	if err != nil {
-		t.Fatal(err)
-	}
+	assert.Nil(t, err, "failed setting guestproperty")
 	if Verbose {
 		t.Logf("OK SetGuestProperty test_key=test_val")
 	}
 
 	val, err := GetGuestProperty(vm, "test_key")
-	if err != nil {
-		t.Fatal(err)
-	}
+	assert.Nil(t, err, "failed getting guestproperty")
 	t.Logf("val='%s'", val)
-	if val != "test_val" {
-		t.Fatal("Wrong value")
-	}
+	assert.Equal(t, val, "test_val", "guestproperty not set to the proper value")
 	if Verbose {
 		t.Logf("OK GetGuestProperty test_key=test_val")
 	}
 
 	// Now deletes it...
 	err = DeleteGuestProperty(vm, "test_key")
-	if err != nil {
-		t.Fatal(err)
-	}
+	assert.Nil(t, err, "failed deleting guestproperty")
 	if Verbose {
 		t.Logf("OK DeleteGuestProperty test_key")
 	}
 
 	// ...and check that it is  no longer readable
 	_, err = GetGuestProperty(vm, "test_key")
-	if err == nil {
-		t.Fatal(fmt.Errorf("Failed deleting guestproperty"))
-	}
+	assert.NotNil(t, err, "guestproperty not deleted")
+
 	if Verbose {
 		t.Logf("OK GetGuestProperty test_key=empty")
 	}
