aacd8eb9f84d7e2c097eaae2b7eb0c441bd03142
Francois-Xavier 'FiX' KOWALSKI <francois-xavier.kowalski@hp.com>
2018-04-22 09:47:17 +0200

    Use require() stop Test instead of assert()

--- guestprop_test.go
+++ guestprop_test.go
@@ -4,11 +4,11 @@ import (
 	"os"
 	"testing"
 
-	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/require"
 )
 
 func TestGuestProperty(t *testing.T) {
-	assert := assert.New(t)
+	require := require.New(t)
 
 	var vm = os.Getenv("TEST_VM")
 	if len(vm) <= 0 {
@@ -18,29 +18,29 @@ func TestGuestProperty(t *testing.T) {
 	t.Logf("Using '%s'", vm)
 
 	err := SetGuestProperty(vm, "test_key", "test_val")
-	assert.Nil(err, "failed setting guestproperty")
+	require.Nil(err, "failed setting guestproperty")
 	if Verbose {
 		t.Logf("OK SetGuestProperty test_key=test_val")
 	}
 
 	val, err := GetGuestProperty(vm, "test_key")
-	assert.Nil(err, "failed getting guestproperty")
+	require.Nil(err, "failed getting guestproperty")
 	t.Logf("val='%s'", val)
-	assert.Equal(val, "test_val", "guestproperty not set to the proper value")
+	require.Equal(val, "test_val", "guestproperty not set to the proper value")
 	if Verbose {
 		t.Logf("OK GetGuestProperty test_key=test_val")
 	}
 
 	// Now deletes it...
 	err = DeleteGuestProperty(vm, "test_key")
-	assert.Nil(err, "failed deleting guestproperty")
+	require.Nil(err, "failed deleting guestproperty")
 	if Verbose {
 		t.Logf("OK DeleteGuestProperty test_key")
 	}
 
 	// ...and check that it is  no longer readable
 	_, err = GetGuestProperty(vm, "test_key")
-	assert.NotNil(err, "guestproperty not deleted")
+	require.NotNil(err, "guestproperty not deleted")
 
 	if Verbose {
 		t.Logf("OK GetGuestProperty test_key=empty")
